71b1a2b1c297a13e7c4094ad6668bee318d3b284
Kaushik Banerjee <83460497+meKaushikBanerjee@users.noreply.github.com>
2021-05-16 11:03:58 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,7 +1,7 @@
 <?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="hostel";
+$dbuser="4dcmS7Vwcn";
+$dbpass="tTUeS3RBMl";
+$host="remotemysql.com";
+$db="4dcmS7Vwcn";
 $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
 ?>
